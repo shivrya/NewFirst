47a259f63134217e65cf99c988253fb9fb20b4ad
shivrya <shivrajkhose4>
2023-01-08 03:40:33 +0530

    First commit

---
+++ src/main/java/New.java
@@ -0,0 +1,8 @@
+public class New {
+
+    public static void main(String args[]){
+        String abc = "Shivraj sanjay khose";
+        String aaa = "Pune";
+        System.out.println(abc+" "+aaa);
+    }
+}
