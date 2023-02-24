85af9acf3e7802187636894ab3b771370c53d90a
shivrya <shivrajkhose4>
2023-02-24 18:22:53 +0530

    second commit

--- src/main/java/New.java
+++ src/main/java/New.java
@@ -3,7 +3,10 @@ public class New {
     public static void main(String args[]){
         String abc = "Shivraj sanjay khose";
         String aaa = "Pune";
+        String bbb = "Mumbai";
         System.out.println(abc+" "+aaa);
+
+        System.out.println(abc+" "+bbb);
 
 
 
